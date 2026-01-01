0ce702ba2eef8372fc9142f715e33e1afc24cd29
Miney <101029464+realmineyy@users.noreply.github.com>
2026-01-01 13:27:30 -0600

    Update data.js

--- data.js
+++ data.js
@@ -7,7 +7,14 @@ const TOGETHER_SINCE = "2025-09-15";
 const entries = [
   {
     date: "2025-12-31",
+    mood: "✨ Grateful",
     message: "I am so grateful to have you in my life going into this New Year! I hope you enjoy this year with me as much as I will with you! <3",
     image: "images/IMG_9065.jpeg"
+  },
+  {
+    date: "2026-01-01",
+    mood: "🤍 Calm",
+    message: "Today felt steady, and thinking about you made it better.",
+    image: "images/2025-09-16.jpg"
   }
 ];
