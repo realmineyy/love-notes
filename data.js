f2bb721a8b821ea4ecde84743ec624405b0f2df7
Miney <101029464+realmineyy@users.noreply.github.com>
2025-12-31 22:45:04 -0600

    Create data.js

---
+++ data.js
@@ -0,0 +1,11 @@
+// Change this once
+const ANNIVERSARY_DATE = "2023-06-14";
+
+// Add new entries to the BOTTOM
+const entries = [
+  {
+    date: "2025-01-01",
+    message: "I love how natural it feels to be with you.",
+    image: "images/2025-01-01.jpg"
+  }
+];
