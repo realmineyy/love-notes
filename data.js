8b0bc2cb01cb06e1dc42766765fac03cb9114204
Miney <101029464+realmineyy@users.noreply.github.com>
2025-12-31 22:57:27 -0600

    Update data.js

--- data.js
+++ data.js
@@ -4,8 +4,8 @@ const ANNIVERSARY_DATE = "2025-09-15";
 // Add new entries to the BOTTOM
 const entries = [
   {
-    date: "2025-01-01",
-    message: "I love how natural it feels to be with you.",
+    date: "2025-12-31",
+    message: "I am so grateful to have you in my life going into this New Year! I hope you enjoy this year with me as much as I will with you! <3",
     image: "images/2025-01-01.jpg"
   }
 ];
