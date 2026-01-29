517abefdb9b38c025f0de7212ca4968f89f8ee8a
Miney <101029464+realmineyy@users.noreply.github.com>
2026-01-28 18:50:16 -0600

    fixed image loading

--- memories.js
+++ memories.js
@@ -1,17 +1,17 @@
 const memories = [
   {
     date: "December 16th, 2025",
-    image: "images/IMG_9064.jpg",
+    image: "images/IMG_9064.jpeg",
     note: "Your first birthday that we had together. I was so happy to be invited to your birthday dinner (even though I had to ask lol). I had a great time talking with your parents, siblings, and friends. Glad I could come!"
   },
   {
     date: "",
-    image: "images/IMG_9230.jpg",
+    image: "images/IMG_9230.JPG",
     note: "This was such a fun night and I am glad I got to see you. I was excited to take the photo even though I look like I was being forced to be there (my smile is lowk bad sometimes)"
   },
   {
     date: "",
-    image: "images/IMG_9296.jpg",
+    image: "images/IMG_9296.JPG",
     note: "I actually really like this photo. I wanted to support you and decided that the hat was a fun thing to do. I hope you actually like this photo!"
   }
 ];
