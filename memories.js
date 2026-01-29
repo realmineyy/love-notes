78523a5bfca13ff85f0d492a34a495042a2a2445
Miney <101029464+realmineyy@users.noreply.github.com>
2026-01-28 18:33:16 -0600

    first edition random memory button, testing features

---
+++ memories.js
@@ -0,0 +1,17 @@
+const memories = [
+  {
+    date: "September 15, 2025",
+    image: "images/memory1.jpg",
+    note: "Our first day together. I still remember how calm everything felt."
+  },
+  {
+    date: "October 3, 2025",
+    image: "images/memory2.jpg",
+    note: "This day made me realize how safe I feel with you."
+  },
+  {
+    date: "November 12, 2025",
+    image: "images/memory3.jpg",
+    note: "I didnt say it enough here, but I was really happy."
+  }
+];
