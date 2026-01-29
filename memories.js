40d20a6752d9b43502091687f0de32582091e71b
Miney <101029464+realmineyy@users.noreply.github.com>
2026-01-28 18:48:17 -0600

    memories feature finished

--- memories.js
+++ memories.js
@@ -1,17 +1,17 @@
 const memories = [
   {
-    date: "September 15, 2025",
-    image: "images/memory1.jpg",
-    note: "Our first day together. I still remember how calm everything felt."
+    date: "December 16th, 2025",
+    image: "images/IMG_9064.jpg",
+    note: "Your first birthday that we had together. I was so happy to be invited to your birthday dinner (even though I had to ask lol). I had a great time talking with your parents, siblings, and friends. Glad I could come!"
   },
   {
-    date: "October 3, 2025",
-    image: "images/memory2.jpg",
-    note: "This day made me realize how safe I feel with you."
+    date: "",
+    image: "images/IMG_9230.jpg",
+    note: "This was such a fun night and I am glad I got to see you. I was excited to take the photo even though I look like I was being forced to be there (my smile is lowk bad sometimes)"
   },
   {
-    date: "November 12, 2025",
-    image: "images/memory3.jpg",
-    note: "I didnt say it enough here, but I was really happy."
+    date: "",
+    image: "images/IMG_9296.jpg",
+    note: "I actually really like this photo. I wanted to support you and decided that the hat was a fun thing to do. I hope you actually like this photo!"
   }
 ];
