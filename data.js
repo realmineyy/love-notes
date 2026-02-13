14c25b67d489356aa9c751e811ce3d17a4724aa9
Miney <101029464+realmineyy@users.noreply.github.com>
2026-02-13 10:41:20 -0600

    changes to design

--- data.js
+++ data.js
@@ -8,266 +8,266 @@ const entries = [
   {
     date: "2025-12-31",
     mood: "✨ Grateful",
-    message: "I am so grateful to have you in my life going into this New Year! I hope you enjoy this year with me as much as I will with you! <3",
+    text: "I am so grateful to have you in my life going into this New Year! I hope you enjoy this year with me as much as I will with you! <3",
     image: "images/IMG_9065.jpeg"
   },
   {
     date: "2026-01-01",
     mood: "🤍 Worried",
-    message: "Hope you're doing okay. Please talk to me if something's wrong. Love you <3",
+    text: "Hope you're doing okay. Please talk to me if something's wrong. Love you <3",
     image: "images/IMG_9296.JPG"
   },
   {
     date: "2026-01-02",
     mood: "Chillin",
-    message: "Even on quiet days, loving you feels like the easiest thing I've ever done. Thank you for being so amazing!",
+    text: "Even on quiet days, loving you feels like the easiest thing I've ever done. Thank you for being so amazing!",
     image: "images/no_image_today.jpg"
   },
   {
     date: "2026-01-03",
     mood: "Chillin",
-    message: "No matter how today looks, I'm grateful to have you in my life <3",
+    text: "No matter how today looks, I'm grateful to have you in my life <3",
     image: "images/no_image_today.jpg"
   },
   {
     date: "2026-01-04",
     mood: "",
-    message: "I miss you a lot. We should hangout soon before I go to Atlanta. I dont even know if you read these, just know that I love you <3",
+    text: "I miss you a lot. We should hangout soon before I go to Atlanta. I dont even know if you read these, just know that I love you <3",
     image: "images/no_image_today.jpg"
   },
   {
     date: "2026-01-05",
     mood: "",
-    message: "I don't need perfect days to know that I love you :)",
+    text: "I don't need perfect days to know that I love you :)",
     image: "images/no_image_today.jpg"
   },
   {
     date: "2026-01-06",
     mood: "",
-    message: "Glad to see you today! Hopefully you enjoyed our time as much as I did <3",
+    text: "Glad to see you today! Hopefully you enjoyed our time as much as I did <3",
     image: "images/no_image_today.jpg"
   },
   {
     date: "2026-01-07",
     mood: "Hilarious",
-    message: "I am dramatic... dramatically in love with you hehe <3",
+    text: "I am dramatic... dramatically in love with you hehe <3",
     image: "images/no_image_today.jpg"
   }
   ,
   {
     date: "2026-01-08",
     mood: "Worried",
-    message: "Sorry you felt bad today, just want to make sure you know I still love you! <3",
+    text: "Sorry you felt bad today, just want to make sure you know I still love you! <3",
     image: "images/no_image_today.jpg"
   },
   {
     date: "2026-01-09",
     mood: "Sorry",
-    message: "I am not very good at words but given the opportunity and some time I can try. You are by far the most amazing thing to ever happen to me. You came in at such a perfect time in my life and brought like a calm to the storm that was my life at the time. At this point in time, you probably feel as if you can't trust me or forgive me, and for that I have no excuse. No excuse being that I have no excuse for making you feel that way when you should always feel as if you can trust me and rely on me. I am sorry that I forgot to write this message sooner, you deserve it. I really hope that you stay with me because I have no idea where I'd be without you. Sincerely, Aiden.",
+    text: "I am not very good at words but given the opportunity and some time I can try. You are by far the most amazing thing to ever happen to me. You came in at such a perfect time in my life and brought like a calm to the storm that was my life at the time. At this point in time, you probably feel as if you can't trust me or forgive me, and for that I have no excuse. No excuse being that I have no excuse for making you feel that way when you should always feel as if you can trust me and rely on me. I am sorry that I forgot to write this message sooner, you deserve it. I really hope that you stay with me because I have no idea where I'd be without you. Sincerely, Aiden.",
     image: "images/no_image_today.jpg"
   },
   {
     date: "2026-01-10",
     mood: "Glad",
-    message: "I am glad you're still with me and I honestly can't believe that you are. I am grateful to have you and happier everyday im with you. Thank you for being in my life. I love you <3",
+    text: "I am glad you're still with me and I honestly can't believe that you are. I am grateful to have you and happier everyday im with you. Thank you for being in my life. I love you <3",
     image: "images/no_image_today.jpg"
   },
   {
     date: "2026-01-11",
     mood: "",
-    message: "You are so incredibly beautiful and I am lucky to have you. I miss you tho, u should come over :) I love you <3",
+    text: "You are so incredibly beautiful and I am lucky to have you. I miss you tho, u should come over :) I love you <3",
     image: "images/no_image_today.jpg"
   },
   {
     date: "2026-01-12",
     mood: "",
-    message: "I love spending time and talking with you, it brightens my day. I love you <3",
+    text: "I love spending time and talking with you, it brightens my day. I love you <3",
     image: "images/no_image_today.jpg"
   },
   {
     date: "2026-01-13",
     mood: "Happy",
-    message: "You are the most beautiful and amazing girl ever! I'm so glad to have you! Love you <3",
+    text: "You are the most beautiful and amazing girl ever! I'm so glad to have you! Love you <3",
     image: "images/no_image_today.jpg"
   },
   {
     date: "2026-01-14",
     mood: "",
-    message: "I'm so lucky to have you in my life, I love you so much <3",
+    text: "I'm so lucky to have you in my life, I love you so much <3",
     image: "images/no_image_today.jpg"
   },
   {
     date: "2026-01-15",
     mood: "",
-    message: "I'm so glad you came over this morning, I love hanging out with you. Sorry for that one thing, won't happen again but figured I'd try something new. Love you <3",
+    text: "I'm so glad you came over this morning, I love hanging out with you. Sorry for that one thing, won't happen again but figured I'd try something new. Love you <3",
     image: "images/no_image_today.jpg"
   },
   {
     date: "2026-01-16",
     mood: "",
-    message: "I think you are beautiful and sweet and kind (sorta) and I hope you know that I will always love you, cherish you, and want the best for you. I love you so much <3",
+    text: "I think you are beautiful and sweet and kind (sorta) and I hope you know that I will always love you, cherish you, and want the best for you. I love you so much <3",
     image: "images/no_image_today.jpg"
   },
   {
     date: "2026-01-17",
     mood: "",
-    message: "I'm gonna miss you a ton when I go back to college. I'm so glad i got to spend so much time with you over winter break. Love you <3",
+    text: "I'm gonna miss you a ton when I go back to college. I'm so glad i got to spend so much time with you over winter break. Love you <3",
     image: "images/no_image_today.jpg"
   },
   {
     date: "2026-01-18",
     mood: "",
-    message: "you are the greatest thing to happen to me and im happy that youve stuck with me even with the distance, i love you",
+    text: "you are the greatest thing to happen to me and im happy that youve stuck with me even with the distance, i love you",
     image: "images/no_image_today.jpg"
   },
   {
     date: "2026-01-19",
     mood: "",
-    message: "I am sorry that I haven't been keeping up with writing you like I should. I finally got it fixed on my phone so that will never happen again. I'm so glad ur in my life and I can't imagine my life without you. I love you <3",
+    text: "I am sorry that I haven't been keeping up with writing you like I should. I finally got it fixed on my phone so that will never happen again. I'm so glad ur in my life and I can't imagine my life without you. I love you <3",
     image: "images/no_image_today.jpg"
   },
   {
     date: "2026-01-20",
     mood: "",
-    message: "You push me to be better every single day. I am so grateful to have you, I love you <3",
+    text: "You push me to be better every single day. I am so grateful to have you, I love you <3",
     image: "images/no_image_today.jpg"
   },
   {
     date: "2026-01-21",
     mood: "",
-    message: "Sorry my video has been delayed, just know you're on my mind always. Love you <3",
+    text: "Sorry my video has been delayed, just know you're on my mind always. Love you <3",
     image: "images/no_image_today.jpg"
   },
   {
     date: "2026-01-22",
     mood: "",
-    message: "I am writing this at 12:41 A.M. I am so sorry that you have a lot going on and I am not doing anything to help. I want you to know that I love you, care for you, and will always be on your side. Please talk to me or ask for help if you need to or just want to chat with me, I would love to talk to you about random stuff all the time. Love Always, Aiden.",
+    text: "I am writing this at 12:41 A.M. I am so sorry that you have a lot going on and I am not doing anything to help. I want you to know that I love you, care for you, and will always be on your side. Please talk to me or ask for help if you need to or just want to chat with me, I would love to talk to you about random stuff all the time. Love Always, Aiden.",
     image: "images/no_image_today.jpg"
   },
   {
     date: "2026-01-23",
     mood: "",
-    message: "I care about you so much and I am sorry that you're going through stuff but I want to be there for you always. JUst know that I am here for you if you need me, please talk to me. I love you <3",
+    text: "I care about you so much and I am sorry that you're going through stuff but I want to be there for you always. JUst know that I am here for you if you need me, please talk to me. I love you <3",
     image: "images/no_image_today.jpg"
   },
   {
     date: "2026-01-24",
     mood: "",
-    message: "I love you so much and I am so glad you're in my life <3",
+    text: "I love you so much and I am so glad you're in my life <3",
     image: "images/no_image_today.jpg"
   },
   {
     date: "2026-01-25",
     mood: "",
-    message: "I'm feeling better and I hope you start to feel better too. Sorry to get you sick digitally (or however you put it), I love you <3",
+    text: "I'm feeling better and I hope you start to feel better too. Sorry to get you sick digitally (or however you put it), I love you <3",
     image: "images/no_image_today.jpg"
   },
   {
     date: "2026-01-26",
     mood: "Empty",
-    message: "Couldn't find the words to write and didn't want to cheat you and make the seas rise any higher. Just know I love you <3",
+    text: "Couldn't find the words to write and didn't want to cheat you and make the seas rise any higher. Just know I love you <3",
     image: "images/no_image_today.jpg"
   },
   {
     date: "2026-01-27",
     mood: "",
-    message: "You are the most beautiful and amazing woman I have ever had the pleasure of being with and I hope that never ends. I love you so much <3",
+    text: "You are the most beautiful and amazing woman I have ever had the pleasure of being with and I hope that never ends. I love you so much <3",
     image: "images/no_image_today.jpg"
   },
   {
     date: "2026-01-28",
     mood: "",
-    message: "I want to be with you for the rest of my life. I have made plenty of mistakes during our relationship but I think you are the smartest, most wonderful, kindest, most beautiful girl in the world and I know that I haven't made you feel as if that is the truth, but I promise if you'll let me I'll dedicate the rest of my life to making sure that you know how amazing you are. I love you so much, stay with me forever. Truly yours, Aiden.",
+    text: "I want to be with you for the rest of my life. I have made plenty of mistakes during our relationship but I think you are the smartest, most wonderful, kindest, most beautiful girl in the world and I know that I haven't made you feel as if that is the truth, but I promise if you'll let me I'll dedicate the rest of my life to making sure that you know how amazing you are. I love you so much, stay with me forever. Truly yours, Aiden.",
     image: "images/no_image_today.jpg"
   },
   {
     date: "2026-01-29",
     mood: "",
-    message: "I think you are the most amazing person in the world and I am so lucky to have you in my life, I love you so much <3",
+    text: "I think you are the most amazing person in the world and I am so lucky to have you in my life, I love you so much <3",
     image: "images/no_image_today.jpg"
   },
   {
     date: "2026-01-30",
     mood: "",
-    message: "You are the best thing to ever happen to me, I love you so much <3",
+    text: "You are the best thing to ever happen to me, I love you so much <3",
     image: "images/no_image_today.jpg"
   },
   {
     date: "2026-01-31",
     mood: "",
-    message: "I am so glad to have you in ym life and I hope you stay with me forever <3",
+    text: "I am so glad to have you in ym life and I hope you stay with me forever <3",
     image: "images/no_image_today.jpg"
   },
   {
     date: "2026-02-01",
     mood: "",
-    message: "Happy Black History Month!! Glad to have a beautiful queen like you in my life <3",
+    text: "Happy Black History Month!! Glad to have a beautiful queen like you in my life <3",
     image: "images/no_image_today.jpg"
   },
   {
     date: "2026-02-02",
     mood: "",
-    message: "I hope you actually enjoy reading these. It means a lot that you actually check this every single day. I put a lot of effort into it and I can appreciate that it means something to you. I love you so much <3",
+    text: "I hope you actually enjoy reading these. It means a lot that you actually check this every single day. I put a lot of effort into it and I can appreciate that it means something to you. I love you so much <3",
     image: "images/no_image_today.jpg"
   },
   {
     date: "2026-02-03",
     mood: "",
-    message: "I appreciate your support everyday and I don't think I could go a day without talking to you. I love you so much <3",
+    text: "I appreciate your support everyday and I don't think I could go a day without talking to you. I love you so much <3",
     image: "images/no_image_today.jpg"
   },
   {
     date: "2026-02-04",
     mood: "",
-    message: "You're the prettiest girl in the world! I love you so much! <3",
+    text: "You're the prettiest girl in the world! I love you so much! <3",
     image: "images/no_image_today.jpg"
   },
   {
     date: "2026-02-05",
     mood: "",
-    message: "I love how independent and strong-willed you are. I can't imagine being with anybody else. I love you <3",
+    text: "I love how independent and strong-willed you are. I can't imagine being with anybody else. I love you <3",
     image: "images/no_image_today.jpg"
   },
   {
     date: "2026-02-06",
     mood: "",
-    message: "I am really excited to be forgiven for my past mistakes. I'm so glad to have you in my life. I love you soooo much <3",
+    text: "I am really excited to be forgiven for my past mistakes. I'm so glad to have you in my life. I love you soooo much <3",
     image: "images/no_image_today.jpg"
   },
   {
     date: "2026-02-07",
     mood: "",
-    message: "I love how smart you are! I love everything about you <3",
+    text: "I love how smart you are! I love everything about you <3",
     image: "images/no_image_today.jpg"
   },
   {
     date: "2026-02-08",
     mood: "",
-    message: "I hope you have the most amazing life! You deserve it for being so perfect. I love you <3",
+    text: "I hope you have the most amazing life! You deserve it for being so perfect. I love you <3",
     image: "images/no_image_today.jpg"
   },
   {
     date: "2026-02-09",
     mood: "",
-    message: "You're going to do so amazing on your ACT and you're gonna be valedictorian! I'm so proud of everything you've accomplished! I love you so much <3",
+    text: "You're going to do so amazing on your ACT and you're gonna be valedictorian! I'm so proud of everything you've accomplished! I love you so much <3",
     image: "images/no_image_today.jpg"
   },
   {
     date: "2026-02-10",
     mood: "",
-    message: "You are the most beautiful, smartest, kindest girl in the world and I NEED you in my life. I love you so much <3",
+    text: "You are the most beautiful, smartest, kindest girl in the world and I NEED you in my life. I love you so much <3",
     image: "images/no_image_today.jpg"
   },
   {
     date: "2026-02-11",
     mood: "",
-    message: "I can't wait to spend the rest of my life with you! I love you so much <3",
+    text: "I can't wait to spend the rest of my life with you! I love you so much <3",
     image: "images/no_image_today.jpg"
   },
   {
     date: "2026-02-12",
     mood: "",
-    message: "You are so graceful and I couldn't have asked for a better partner! I love you <3",
+    text: "You are so graceful and I couldn't have asked for a better partner! I love you <3",
     image: "images/no_image_today.jpg"
   }
 ];
--- script.js
+++ script.js
@@ -1,45 +1,52 @@
-// Dark mode toggle
-const toggle = document.getElementById("darkToggle");
-toggle.addEventListener("click", () => {
+// Dark mode
+document.getElementById("darkToggle").onclick = () =>
   document.body.classList.toggle("dark");
-});
 
-// Relationship dates
+// Relationship timing
 const startDate = new Date("2025-09-15T00:00:00");
 
 function updateTime() {
   const now = new Date();
   const diff = now - startDate;
 
-  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
-  const minutes = Math.floor((diff / (1000 * 60)) % 60);
-
+  const days = Math.floor(diff / 86400000);
   document.getElementById("togetherTime").textContent =
-    `We’ve been together for ${days} days, ${hours} hours, and ${minutes} minutes.`;
+    `We’ve been together for ${days} days 💙`;
 
   const nextAnniversary = new Date(now.getFullYear(), 8, 15);
   if (now > nextAnniversary) nextAnniversary.setFullYear(now.getFullYear() + 1);
 
-  const daysLeft = Math.ceil((nextAnniversary - now) / (1000 * 60 * 60 * 24));
-
+  const daysLeft = Math.ceil((nextAnniversary - now) / 86400000);
   document.getElementById("anniversaryCountdown").textContent =
-    `${daysLeft} days until our anniversary 💙`;
+    `${daysLeft} days until our anniversary`;
 }
 
-setInterval(updateTime, 60000);
 updateTime();
+setInterval(updateTime, 60000);
+
+// Daily message
+const today = messages[0];
+document.getElementById("todayDate").textContent = today.date;
+document.getElementById("todayMessage").textContent = today.text;
+
+// Archive
+const archive = document.getElementById("messageArchive");
+messages.slice(1).forEach(msg => {
+  const div = document.createElement("div");
+  div.className = "archive-entry";
+  div.innerHTML = `
+    <p class="archive-date">${msg.date}</p>
+    <p>${msg.text}</p>
+  `;
+  archive.appendChild(div);
+});
 
 // Random memory
-const randomBtn = document.getElementById("randomMemoryBtn");
-const card = document.getElementById("memoryDisplay");
+document.getElementById("randomMemoryBtn").onclick = () => {
+  const m = memories[Math.floor(Math.random() * memories.length)];
+  document.getElementById("memoryImage").src = m.image;
+  document.getElementById("memoryDate").textContent = m.date;
+  document.getElementById("memoryNote").textContent = m.note;
+  document.getElementById("memoryDisplay").classList.remove("hidden");
+};
 
-randomBtn.addEventListener("click", () => {
-  const random = memories[Math.floor(Math.random() * memories.length)];
-
-  document.getElementById("memoryImage").src = random.image;
-  document.getElementById("memoryDate").textContent = random.date;
-  document.getElementById("memoryNote").textContent = random.note;
-
-  card.classList.remove("hidden");
-});
